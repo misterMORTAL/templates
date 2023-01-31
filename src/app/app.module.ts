16f86a25db6a5b880f3d0fe0a7ef6ec066eed540
misterMORTAL <102058753+misterMORTAL@users.noreply.github.com>
2023-01-30 21:35:48 -0500

    templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { AuthComponent } from './auth/auth.component';
+import { SharedComponent } from './shared/shared.component';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+import { NoFoundComponent } from './no-found/no-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    AuthComponent,
+    SharedComponent,
+    NoFoundComponent,  
   ],
   imports: [
-    BrowserModule
+    PagesModule,
+    BrowserModule,
+    AuthModule,
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
